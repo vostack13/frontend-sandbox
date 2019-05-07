afdea359b4ab6ad076bb42e206b7f0a0f1684988
vostack13 <stanislav.vozikov@gmail.com>
2019-05-07 20:18:47 +0300

    Fix swipe

--- slider-vanila-js/src/js/index.js
+++ slider-vanila-js/src/js/index.js
@@ -4,11 +4,16 @@ const sliderItem = document.getElementById('general-slider__item');
 const buttonPrev = document.querySelector('.btn__prev');
 const buttonNext = document.querySelector('.btn__next');
 
+let transition = 0;
 let width = 0;
 let countViewItem = 0;
 let positionSlider = 0;
 let maxPositionSlider = sliderContainer.firstElementChild.childElementCount;
 let translate = 0;
+let posX1 = 0;
+let posX2 = 0;
+let posInitial;
+let posFinal;
 
 const setWidthItem = () => {
 	if (window.innerWidth > 1024)
@@ -26,73 +31,70 @@ const setWidthItem = () => {
 }
 
 const shiftSlider = value => {
-	if (positionSlider + value > 0 || positionSlider + value < countViewItem - maxPositionSlider)
-		return;
+	
+	// if (positionSlider + value > 0 || positionSlider + value < countViewItem - maxPositionSlider)
+	// return;
+
+	if (positionSlider + value > 0) {
+		console.log('>0');
+		
+		return sliderContainer.firstElementChild.style.cssText = `--translate: translateX(${0}px)`;
+	}
+	
+	if(positionSlider + value < countViewItem - maxPositionSlider) {
+		console.log('< 0');
+		return sliderContainer.firstElementChild.style.cssText = `--translate: translateX(${width * (countViewItem - maxPositionSlider)}px)`;
+	}
 	
 	positionSlider = positionSlider + value;
+	// console.log('positionSlider', positionSlider);
+	transition = width * positionSlider
 	sliderContainer.firstElementChild.style.cssText = `--translate: translateX(${width * positionSlider}px)`;
 }
 
-window.addEventListener('DOMContentLoaded', setWidthItem);
-window.addEventListener('resize', setWidthItem);
-buttonPrev.addEventListener('click', () => shiftSlider(-1))
-buttonNext.addEventListener('click', () => shiftSlider(1))
-
-// Touch events
-let posX1 = 0;
-let posX2 = 0;
-let posInitial;
-let posFinal;
-
-const dragStart = event => {
-	// event.stopPropagation();
+const dragStart = event =>  {
 	event.preventDefault();
-	posInitial = event.touches[0].clientX;
-
-	posX1 = event.touches[0].clientX;
-
-	// console.log('posInitial', posInitial);
+	event.stopPropagation();
+	posInitial = parseInt(event.changedTouches[0].clientX);
 }
 
 const dragAction = event => {
-	// event.stopPropagation();
-	// event.preventDefault();
-	// console.log(event.touches[0]);
+	event.preventDefault();
+	event.stopPropagation();
+	posX2 = posInitial - parseInt(event.changedTouches[0].clientX);
+	posFinal = parseInt(event.changedTouches[0].clientX);
 
-	posX2 = posX1 - event.touches[0].clientX;
-	posFinal = event.touches[0].clientX;
-
-	// console.log('dragAction ', translate);
-	
-	// sliderContainer.firstElementChild.style.cssText = `--translate: translateX(${sliderContainer.firstElementChild.offsetLeft - posX2}px)`;
+	sliderContainer.firstElementChild.style.cssText = `--translate: translateX(${transition - posX2}px)`;
 }
 
 const dragEnd = event => {
-	// posFinal = sliderContainer.offsetLeft;
-
+	event.preventDefault();
+	event.stopPropagation();
+	// console.log('posInitial', posInitial);
+	// console.log('posFinal', posFinal);
 	if (posInitial - posFinal > 0) {
-		touchValue = Math.round(((posInitial - posFinal) / width) * -1);
-		console.log('posFinal', touchValue - 1);
-		shiftSlider(touchValue - 1);
+		
+		touchValue = Math.round((((posInitial - posFinal) * 2) / width) * -1);
+		console.log('touchValue <-', touchValue);
+		shiftSlider(touchValue);
 	} else {
-		touchValue = Math.round((posFinal - posInitial) / width);
-		// console.log('posFinal', touchValue - 1);
-		shiftSlider(touchValue + 1);
+		touchValue = Math.round(((posFinal - posInitial) * 2) / width);
+		console.log('touchValue ->', touchValue);
+		shiftSlider(touchValue);
 	}
-
-
-	// positionSlider = positionSlider + touchValue + 1;
-	
-	// console.log('touchValue ', touchValue);
-	// console.log('positionSlider ', positionSlider);
-	// if (touchValue < 0) {
-	// } else if (touchValue > 0) {
-	// 	shiftSlider(touchValue);
-	// } else {
-	// 	shiftSlider(0)
-	// }
 }
 
-sliderContainer.firstElementChild.addEventListener('touchstart', event => dragStart(event));
-sliderContainer.firstElementChild.addEventListener('touchmove', event => dragAction(event));
-sliderContainer.firstElementChild.addEventListener('touchend', event => dragEnd(event));
+window.addEventListener('DOMContentLoaded', function() {
+	setWidthItem();
+
+	buttonPrev.addEventListener('click', () => shiftSlider(-1))
+	buttonNext.addEventListener('click', () => shiftSlider(1))
+
+	sliderContainer.firstElementChild.addEventListener('touchstart', dragStart, false);
+	sliderContainer.firstElementChild.addEventListener('touchmove', dragAction, false);
+	sliderContainer.firstElementChild.addEventListener('touchend', dragEnd, false);
+
+	document.body.addEventListener('touchmove', function(e){ e.preventDefault(); }, {passive: false});
+});
+
+window.addEventListener('resize', setWidthItem);
